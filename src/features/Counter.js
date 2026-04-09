a89c520910df2a08180dd6df11d5ef202c85faac
Hanan <Hanan.bayazeed56@gmail.com>
2026-04-09 18:44:06 +0300

    first commit

---
+++ src/features/Counter.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./counterSlice";
+
+const Counter = () => {
+  const count = useSelector((state) => {
+    return state.counter.count;
+  });
+  const dispatch = useDispatch();
+
+  return (
+    <section>
+      <h1>Counter: {count}</h1>
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}
+      >
+        -
+      </button>
+    </section>
+  );
+};
+
+export default Counter;
