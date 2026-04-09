e1c560953ce1d9313711c4aac032ab7dbc23eb32
Hanan <Hanan.bayazeed56@gmail.com>
2026-04-09 18:56:24 +0300

    feat(counter/counterSlice)

--- src/features/Counter.js
+++ src/features/Counter.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./counterSlice";
+import { increment, decrement, reset, incrementByAmount } from "./counterSlice";
+import { useState } from "react";
 
 const Counter = () => {
   const count = useSelector((state) => {
     return state.counter.count;
   });
+  const [incrementAmount, setIncrementAmount] = useState(0);
   const dispatch = useDispatch();
+  const incrementValue = Number(incrementAmount) || 0;
+  const resetAll = () => {
+    setIncrementAmount(0);
+    dispatch(reset());
+  };
 
   return (
     <section>
@@ -25,6 +32,23 @@ const Counter = () => {
       >
         -
       </button>
+      <input
+        type="text"
+        value={incrementAmount}
+        onChange={(e) => {
+          setIncrementAmount(e.target.value);
+        }}
+      />
+      <div>
+        <button
+          onClick={() => {
+            dispatch(incrementByAmount(incrementValue));
+          }}
+        >
+          Increment by {incrementValue}
+        </button>
+        <button onClick={resetAll}>Reset All</button>
+      </div>
     </section>
   );
 };
